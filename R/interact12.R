e1e6b2299e8d55e00b6065db3826236b4b5cca56
antonyagonzalez <agonzalez@psych.ubc.ca>
2016-12-02 15:32:24 -0800

    interact12()

--- R/interact12.R
+++ R/interact12.R
@@ -1,3 +1,16 @@
+#' Extract regression model data for 2 way interaction (predictor 1 & 2)
+#'
+#' Pull the regression coefficient, standard error, t-value, p-value and confidence
+#' interval for the two way interaction between the first and second predictors
+#' specified in your linear model.
+#'
+#' @param lm linear model
+#'
+#' @return beta, se, t-value, p-value, ci95_low, ci95_high
+#' @export
+#' @examples
+#' example <- lm(formula = Sepal.Length ~ Petal.Length * Petal.Width * Sepal.Width, data = iris)
+#' interact12(example)
 interact12 <- function(lm) {
   modelsum <- summary(lm)
   modelci <- confint(lm, level = 0.95, method = "boot")
