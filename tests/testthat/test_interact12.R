3e07050a171eb6db374b19ead521f587f9f62115
antonyagonzalez <agonzalez@psych.ubc.ca>
2016-12-02 15:52:23 -0800

    test interact12()

--- tests/testthat/test_interact12.R
+++ tests/testthat/test_interact12.R
@@ -1,11 +1,11 @@
-context("Binding factors")
+context("Extract interaction btw predictors 1 and 2")
 
-test_that("fbind binds factor (or character)", {
-  x <- c('a', 'b')
-  x_fact <- factor(x)
-  y <- c('c', 'd')
-  z <- factor(c('a', 'b', 'c', 'd'))
+test_that("Correct beta is being pulled", {
+  iris_model <- lm(formula = Sepal.Length ~ Petal.Length * Petal.Width * Sepal.Width, data = iris)
+  iris_sum <- summary(iris_model)
+  x <- iris_sum$coefficients[5]
+  iris_interact <- interact12(iris_model)
+  y <- iris_interact[[1]]
 
-  expect_identical(fbind(x, y), z)
-  expect_identical(fbind(x_fact, y), z)
+  expect_identical(x, y)
 })
