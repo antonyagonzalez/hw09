0b5f681121723a9b26c21924918a395e0a7c6f85
antonyagonzalez <agonzalez@psych.ubc.ca>
2016-12-03 15:10:35 -0800

    3rd test interact3()

---
+++ tests/testthat/test3_interact3.R
@@ -0,0 +1,11 @@
+context("Extract three-way interaction")
+
+test_that("Correct upper confidence interval given", {
+  iris_model <- lm(formula = Sepal.Length ~ Petal.Length * Petal.Width * Sepal.Width, data = iris)
+  iris_ci <- confint(iris_model, level = 0.95, method = "boot")
+  x <- iris_ci[8, 2]
+  iris_interact <- interact3(iris_model)
+  y <- iris_interact[6]
+
+  expect_equivalent(x, y)
+})
