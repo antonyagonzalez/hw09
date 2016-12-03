c93cb25437546546e65b0adf2cd02cd209fdf611
antonyagonzalez <agonzalez@psych.ubc.ca>
2016-12-03 15:22:24 -0800

    package description

---
+++ R/hw09-package.r
@@ -0,0 +1,15 @@
+#' hw09: a mini R package by antonyagonzalez
+#'
+#' The following package is an excerpt of a larger package I am working on focusing on
+#' making regression coefficients easier to deal with. Looking over regression output
+#' and trying to extract what you need from it can be quite the headache, especially if
+#' you have many different models to deal with. This mini package will extract betas
+#' and their relevant statistics from 3 way interaction regression models to get
+#' information for a 2 way interaction between your 1st and 2nd predictor (interact12),
+#' information for the 3 way interaction (interact3), and will meta-analyze standardized
+#' regression coefficients that you input.
+#'
+#'
+#' @name hw09
+#' @docType package
+NULL
