3a41f36a1013bd5c4822057f98d15983c375d384
antonyagonzalez <agonzalez@psych.ubc.ca>
2016-12-02 16:11:36 -0800

    2nd test interact12()

--- tests/testthat/test2_interact12.R
+++ tests/testthat/test2_interact12.R
@@ -3,5 +3,5 @@ context("Extract interaction btw predictors 1 and 2")
 test_that("Function does not work using a vector", {
   vector <- c(1:12)
 
-  expect_error(interact12(vector))
+  expect_that((interact12(vector)), throws_error("operator is invalid for atomic vectors"))
 })
